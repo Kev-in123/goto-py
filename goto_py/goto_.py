f95c45cbe5fbdd579b78dbe1a6a61b7a7245cae0
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-14 13:01:38 -0400

    Update goto_.py

--- goto_py/goto_.py
+++ goto_py/goto_.py
@@ -7,12 +7,15 @@ def run_once(f):
         if not wrapper.has_run:
             wrapper.has_run = True
             return f(*args, **kwargs)
+
     wrapper.has_run = False
     return wrapper
 
 @run_once
-def goto(line):
+def goto(line, once_only = False):
   caller = inspect.stack()[1]
   with open(FILENAME, "r") as f:
     contents = "".join(f.readlines()[line-1:])
+  goto.has_run = once_only
   exec(contents, caller.frame.f_globals)
+  
