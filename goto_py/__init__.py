3db9f64561d81e76f05f53feece76c383b2443fa
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-14 13:17:10 -0400

    Add files via upload

--- goto_py/__init__.py
+++ goto_py/__init__.py
@@ -2,6 +2,6 @@ __author__ = "Kev-in123"
 __title__ = "goto-py"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Kev-in123"
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 from .goto_ import goto
--- goto_py/goto_.py
+++ goto_py/goto_.py
@@ -1,13 +1,13 @@
 import inspect
- 
+
 FILENAME = inspect.stack()[-1].filename
+
 
 def run_once(f):
     def wrapper(*args, **kwargs):
         if not wrapper.has_run:
             wrapper.has_run = True
             return f(*args, **kwargs)
-
     wrapper.has_run = False
     return wrapper
 
@@ -18,4 +18,4 @@ def goto(line, once_only = False):
     contents = "".join(f.readlines()[line-1:])
   goto.has_run = once_only
   exec(contents, caller.frame.f_globals)
-  
+
